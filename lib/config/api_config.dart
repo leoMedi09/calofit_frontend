72c2f4346812571d92bef9adbb13c7c98bab0c33
leoMedi09 <leomedinaflores09@gmail.com>
2026-02-21 17:25:29 -0500

    avance

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -9,9 +9,9 @@ class ApiConfig {
       // Para Flutter Web
       return 'http://localhost:8000';
     } else if (Platform.isAndroid) {
-      // Para dispositivo físico Android en WiFi / Ethernet (IP actualizada: 192.168.15.7)
+      // Para dispositivo físico Android en WiFi / Ethernet (IP actualizada: 192.168.15.17)
       // Si usas el EMULADOR de Android Studio, usa: 'http://10.0.2.2:8000'
-      return 'http://192.168.15.7:8000'; 
+      return 'http://192.168.15.17:8000'; 
     } else if (Platform.isIOS) {
       // Para iOS Simulator
       return 'http://localhost:8000';
